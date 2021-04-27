ba17a45ef324b820f518ef85a8c75dbd4181a687
joongkyu-park <jk.park.tech@gmail.com>
2021-04-28 04:19:40 +0900

    move-app with node.js and react

---
+++ client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL='http://api.themoviedb.org/3/';
+export const API_KEY = '83b6b0defcc4d28d11782b9a5905e379';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
